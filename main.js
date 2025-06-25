13a98ef21ea1142fb398d2fb08ce710d37a0ed78
jbyerly <47181989+jbyerly@users.noreply.github.com>
2025-06-25 16:32:05 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+const navbarToggle = navbar.querySelector('#navbar-toggle');
+let isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
+
+const toggleNavbarVisibility = () => {
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute('aria-expanded', isNavbarExpanded);
+};
+
+navbarToggle.addEventListener('click', toggleNavbarVisibility);
+
+const navbarMenu = document.querySelector('#navbar-menu');
+const navbarLinksContainer = navbarMenu.querySelector('.navbar-links');
+
+navbarLinksContainer.addEventListener('click', (e) => e.stopPropagation());
+navbarMenu.addEventListener('click', toggleNavbarVisibility);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const calendarEl = document.getElementById('calendar');
+  
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      events: [
+        {
+          title: 'Sunday Service',
+          date: '2025-05-04',
+          color: 'var(--primary-color)'
+        },
+        {
+          title: 'Bible Study',
+          date: '2025-05-07',
+          color: '#29333a'
+        },
+        {
+            title: 'Sunday Service',
+            date: '2025-05-11',
+            color: 'var(--primary-color)'
+          },
+          {
+            title: 'Bible Study',
+            date: '2025-05-14',
+            color: '#29333a'
+          }
+        // Add more event objects here as needed
+      ],
+      // Additional configuration such as eventClick or dateClick can enhance interactivity.
+      eventClick: function(info) {
+        alert('Event: ' + info.event.title + starttime);
+        // Optionally, display more details or link to an event page.
+      }
+    });
+  
+    calendar.render();
+  });
+  
+
